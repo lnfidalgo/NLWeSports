1cc60329b983ab5a666e9b9a04793e40e763ee95
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-09-13 21:44:15 -0300

    .

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,69 @@
 import './styles/main.css' 
+import logoImg from './assets/logo.svg'
 
 function App() {
   return (
-    <div className='w-8 h-8 bg-black dark:bg-slate-500'>
-      
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" />
+      <h1 className="text-6xl font-black text-white mt-20">
+        Seu <span className="bg-nlw-gradient bg-clip-text text-transparent">duo</span> está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/lol.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+          <strong className="font-bold text-white block">League of Legends</strong>
+          <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/wow.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+          <strong className="font-bold text-white block">World of Warcraft</strong>
+          <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/apex.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+          <strong className="font-bold text-white block">Apex Legends</strong>
+          <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/fortnite.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+          <strong className="font-bold text-white block">Fortnite</strong>
+          <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/cs.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+          <strong className="font-bold text-white block">Counter-Strike: GO</strong>
+          <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/dota.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+          <strong className="font-bold text-white block">Dota 2</strong>
+          <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+      </div>
+
+      <div className="py-6 px-8 self-stretch bg-[#2A2634] mt-8 rounded-lg">
+      <div className="relative">
+        <h2 className="">Não encontrou seu duo?</h2>
+        <span>Publique um anúncio para encontrar novos players!</span>
+      </div>
+      <div className="relative">
+        <h2 className="">Não encontrou seu duo?</h2>
+        <span>Publique um anúncio para encontrar novos players!</span>
+      </div>
+      </div>
     </div>
   )
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,7 +5,13 @@ module.exports = {
     './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 2.08%, #43E7AD 63.94%, #E1D55D 84.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+      },
+    },
   },
   plugins: [],
 }
