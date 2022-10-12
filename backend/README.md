be4c85db299022806a024a13162225e0d4f6894d
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-10-12 15:11:56 -0300

    finalizar NLW

---
+++ backend/README.md
@@ -0,0 +1,28 @@
+# Back-end
+
+## Entidades
+
+### Game
+
+id
+title
+bannerUrl
+
+### Ad
+
+id
+gameId
+name
+yearsPlaying
+discord
+weekDays
+hoursStart
+hoursEnd
+useVoiceChannel
+createdAt
+
+## Casos de uso
+- Listagem de gems com contagem de anúncios
+- Criação de bivi abúncio
+- Listagem de anúncios por game
+- Buscar discord pelo ID do anúncio
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,73 @@
-import express from 'express'
+import express, { query, request, response } from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
+const prisma = new PrismaClient({
+  log: ['query']
+})
 
-app.get('/ads', (request, response) => {
-  return response.json([
-    { id: 1, name: 'Anúncio 1'},
-    { id: 2, name: 'Anúncio 2'},
-    { id: 3, name: 'Anúncio 3'},
-    { id: 4, name: 'Anúncio 4'},
-  ])
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true
+        }
+      }
+    }
+  })
+
+  return response.json(games)
+})
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hoursStart: true,
+      hoursEnd: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    }
+  })
+
+  return response.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(',')
+    }
+  }))
+})
+
+app.get('/ads/:id/discord', async (request, response) => {
+  const adId = request.params.id
+
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true
+    },
+    where: {
+      id: adId,
+    }
+  })
+
+  return response.json({
+    discord: ad.discord
+  })
 })
 
 app.listen(3333)
